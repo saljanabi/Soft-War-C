8fdb126252ed3083a4fd462b81dbf801c9f442fa
aguillemot <guille_a@etna-alternance.net>
2018-08-31 13:44:05 +0200

    hekko

--- functions.c
+++ functions.c
@@ -55,42 +55,39 @@ char nametag(char* identifiant)
 int selfid(Joueur joueur)
 {
 	printf("%s", joueur.nametag);
-	return 0;
 }
 
 int selfstats(Joueur joueur) 
 {
 	printf("%s", joueur.point_d_energie);
-	return 0;
 }
-/*
+
 int gather(Joueur joueur) 
 {
-	if tab[joueur.pos_y][joueur.pos_x] == "energy"
+	return 1;
 }
 
 int next() 
 {
-
+    printf("TODO");    
+    return 1;
 }
-*/
 
 
-
-/*int leftfwd(char** tab, float point_d_action, int orientation, Joueur joueur, int i, int j) 
+int leftfwd(char** tab, float point_d_action, int orientation, Joueur joueur, int i, int j) 
 {
 	if (orientation == 0) 
 	{
 		if (j - 1 >= 0)
 		{
-			if (tab[i][j - 1] == "joueur" )
+			if (tab[i][j - 1] == 'j' )
 			{
 				printf("ko\n"); 
 			}
 			else
 			{
-				tab[i][j - 1] = "joueur";
-				tab[i][j] = "empty";
+				tab[i][j - 1] = 'j';
+				tab[i][j] = 'e';
 				joueur.pos_x--;
 				orientation = (orientation + 3) % 4; 
 				point_d_action--;
@@ -106,14 +103,14 @@ int next()
 	{
 		if (i - 1 >= 0)
 		{
-			if (tab[i - 1][j] == "joueur" ) 
+			if (tab[i - 1][j] == 'j' ) 
 			{
 				printf("ko\n");
 			}
 			else 
 			{
-				tab[i - 1][j] = "joueur";
-				tab[i][j] = "empty";
+				tab[i - 1][j] = 'j';
+				tab[i][j] = 'e';
 				joueur.pos_y--;
 				orientation = (orientation + 3) % 4; 
 				point_d_action--;
@@ -129,14 +126,14 @@ int next()
 	{
 		if (j + 1 << 10)
 		{
-			if (tab[i][j + 1] == "joueur" ) 
+			if (tab[i][j + 1] == 'j') 
 			{
 				printf("ko\n");
 			}
 			else 
 			{
-				tab[i][j + 1] = "joueur";
-				tab[i][j] = "empty";
+				tab[i][j + 1] = 'j';
+				tab[i][j] = 'e';
 				joueur.pos_x++;
 				orientation = (orientation + 3) % 4; 
 				point_d_action--;
@@ -152,14 +149,14 @@ int next()
 	{
 		if (i + 1 << 10)
 		{
-			if (tab[i + 1][j] == "joueur") 
+			if (tab[i + 1][j]== 'j') 
 			{
 				printf("ko\n");
 			}
 			else 
 			{
-				tab[i + 1][j] = "joueur";
-				tab[i][j] = "empty";
+				tab[i + 1][j] = 'j';
+				tab[i][j] = 'e';
 				joueur.pos_y++;
 				orientation = (orientation + 3) % 4; 
 				point_d_action--;
@@ -182,14 +179,14 @@ int rightfwd(char** tab, float point_d_action, int orientation, Joueur joueur, i
 	{
 		if (j + 1 <= 10)
 		{
-			if (tab[i][j + 1] == "joueur" ) 
+			if (tab[i][j + 1] == 'j') 
 			{
 				printf("ko\n"); 
 			}
 			else 
 			{
-				tab[i][j + 1] = "joueur";
-				tab[i][j] = "empty";
+				tab[i][j + 1] = 'j';
+				tab[i][j] = 'e';
 				joueur.pos_x--;
 				orientation = (orientation + 3) % 4; 
 				point_d_action--;
@@ -205,14 +202,14 @@ int rightfwd(char** tab, float point_d_action, int orientation, Joueur joueur, i
 	{
 		if (i + 1 <= 10)
 		{
-			if (tab[i + 1][j] == "joueur" ) 
+			if (tab[i + 1][j] == 'j' ) 
 			{
 				printf("ko\n");
 			}
 			else 
 			{
-				tab[i + 1][j] = "joueur";
-				tab[i][j] = "empty";
+				tab[i + 1][j] = 'j';
+				tab[i][j] = 'e';
 				joueur.pos_y--;
 				orientation = (orientation + 3) % 4; 
 				point_d_action--;
@@ -228,14 +225,14 @@ int rightfwd(char** tab, float point_d_action, int orientation, Joueur joueur, i
 	{
 		if (j - 1 >= 0)
 		{
-			if (tab[i][j - 1] == "joueur" ) 
+			if (tab[i][j - 1] == 'j' ) 
 			{
 				printf("ko\n");
 			}
 			else 
 			{
-				tab[i][j - 1] = "joueur";
-				tab[i][j] = "empty";
+				tab[i][j - 1] = 'j';
+				tab[i][j] = 'e';
 				joueur.pos_x++;
 				orientation = (orientation + 3) % 4; 
 				point_d_action--;
@@ -251,14 +248,14 @@ int rightfwd(char** tab, float point_d_action, int orientation, Joueur joueur, i
 	{
 		if (i - 1 >= 0)
 		{
-			if (tab[i - 1][j] == "joueur") 
+			if (tab[i - 1][j] == 'j') 
 			{
 				printf("ko\n");
 			}
 			else 
 			{
-				tab[i - 1][j] = "joueur";
-				tab[i][j] = "empty";
+				tab[i - 1][j] = 'j';
+				tab[i][j] = 'e';
 				joueur.pos_y++;
 				orientation = (orientation + 3) % 4; 
 				point_d_action--;
@@ -281,14 +278,14 @@ int forward(char** tab, float point_d_action, int orientation, Joueur joueur, in
 			printf("ko\n");
 			
 		}
-		else if (tab[i - 1][j] == "empty")
+		else if (tab[i - 1][j] == 'e')
 		{
-			tab[i - 1][j] = "joueur";
-			tab[i][j] = "empty";
+			tab[i - 1][j] = 'j';
+			tab[i][j] = 'e';
 			joueur.pos_y--;
-			point_d_action = point_d_action - 0.5;/
+			point_d_action = point_d_action - 0.5;
 		}
-		else if (tab[i - 1][j] = "joueur")
+		else if (tab[i - 1][j] == 'j')
 		{
 			printf("Mouvement impossible, vous ne pouvez pas prendre la place d'un adversaire\n");
 		}		
@@ -301,14 +298,14 @@ int forward(char** tab, float point_d_action, int orientation, Joueur joueur, in
 		{
 			printf("ko\n");
 		}
-		else if (tab[i][j + 1] == "empty")
+		else if (tab[i][j + 1] == 'e')
 		{
-			tab[i][j + 1] = "joueur";
-			tab[i][j] = "empty";
+			tab[i][j + 1] = 'j';
+			tab[i][j] = 'e';
 			joueur.pos_x++;
 			point_d_action = point_d_action - 0.5;
 		}
-		else if (tab[i - 1][j] = "joueur")
+		else if (tab[i - 1][j] == 'j')
 		{
 			printf("Mouvement impossible, vous ne pouvez pas prendre la place d'un adversaire\n");
 		}		
@@ -320,14 +317,14 @@ int forward(char** tab, float point_d_action, int orientation, Joueur joueur, in
 		{
 			printf("ko\n");
 		}
-		else if (tab[i + 1][j] == "empty")
+		else if (tab[i + 1][j] == 'e')
 		{
-			tab[i + 1][j] = "joueur";
-			tab[i][j] = "empty";
+			tab[i + 1][j] = 'j';
+			tab[i][j] = 'e';
 			joueur.pos_y++;
 			point_d_action = point_d_action - 0.5;
 		}
-		else if (tab[i + 1][j] = "joueur")
+		else if (tab[i + 1][j] ==  'j')
 		{
 			printf("Mouvement impossible, vous ne pouvez pas prendre la place d'un adversaire\n");
 		}		
@@ -340,14 +337,14 @@ int forward(char** tab, float point_d_action, int orientation, Joueur joueur, in
 		{
 			printf("ko\n");
 		}
-		else if (tab[i][j - 1] == "empty")
+		else if (tab[i][j - 1] ==  'e')
 		{
-			tab[i][j - 1] = "joueur";
-			tab[i][j] = "empty";
+			tab[i][j - 1] = 'j';
+			tab[i][j] = 'e';
 			joueur.pos_x--;
 			point_d_action = point_d_action - 0.5;
 		}
-		else if (tab[i][j - 1] = "joueur")
+		else if (tab[i][j - 1] == 'j')
 		{
 			printf("Mouvement impossible, vous ne pouvez pas prendre la place d'un adversaire\n");
 		}		
@@ -358,7 +355,7 @@ int forward(char** tab, float point_d_action, int orientation, Joueur joueur, in
 
 
 
-int backward(int tab[i][j], float point_d_action, int orientation)
+int backward(char **tab, float point_d_action, int orientation,Joueur joueur,int i,int j)
 {
 	if (orientation == 0)
 	{
@@ -366,14 +363,14 @@ int backward(int tab[i][j], float point_d_action, int orientation)
 		{
 			printf("ko\n");
 		}
-		else if (tab[i - 1][j] == "empty")
+		else if (tab[i - 1][j]== 'e')
 		{
-			tab[i + 1][j] = "joueur";
-			tab[i][j] = "empty";
+			tab[i + 1][j] = 'j';
+			tab[i][j] = 'e';
 			joueur.pos_y++;
-			point_d_action = point_d_action - 0.5;/
+			point_d_action = point_d_action - 0.5;
 		}
-		else if (tab[i - 1][j] = "joueur")
+		else if (tab[i - 1][j] == 'j')
 		{
 			printf("Mouvement impossible, vous ne pouvez pas prendre la place d'un adversaire\n");
 		}		
@@ -386,14 +383,14 @@ int backward(int tab[i][j], float point_d_action, int orientation)
 		{
 			printf("ko\n");
 		}
-		else if (tab[j - 1][j] == "empty")
+		else if (tab[j - 1][j] == 'e')
 		{
-			tab[i][j - 1] = "joueur";
-			tab[i][j] = "empty";
+			tab[i][j - 1] = 'j';
+			tab[i][j] = 'e';
 			joueur.pos_x--;
-			point_d_action = point_d_action - 0.5;/
+			point_d_action = point_d_action - 0.5;
 		}
-		else if (tab[i][j - 1] = "joueur")
+		else if (tab[i][j - 1] =='j')
 		{
 			printf("Mouvement impossible, vous ne pouvez pas prendre la place d'un adversaire\n");
 		}		
@@ -406,14 +403,14 @@ int backward(int tab[i][j], float point_d_action, int orientation)
 			printf("ko\n");
 			
 		}
-		else if (tab[i][j- 1] == "empty")
+		else if (tab[i][j- 1] == 'e')
 		{
-			tab[i][j - 1] = "joueur";
-			tab[i][j] = "empty";
+			tab[i][j - 1] = 'j';
+			tab[i][j] = 'e';
 			joueur.pos_y--;
-			point_d_action = point_d_action - 0.5;/
+			point_d_action = point_d_action - 0.5;
 		}
-		else if (tab[i - 1][j] = "joueur")
+		else if (tab[i - 1][j] ==  'j')
 		{
 			printf("Mouvement impossible, vous ne pouvez pas prendre la place d'un adversaire\n");
 		}				
@@ -426,19 +423,19 @@ int backward(int tab[i][j], float point_d_action, int orientation)
 		{
 			printf("ko\n");
 		}
-		else if (tab[i][j + 1] == "empty")
+		else if (tab[i][j + 1] == 'e')
 		{
-			tab[i][j + 1] = "joueur";
-			tab[i][j] = "empty";
+			tab[i][j + 1] = 'j';
+			tab[i][j] = 'e';
 			joueur.pos_x++;
-			point_d_action = point_d_action - 0.5;/
+			point_d_action = point_d_action - 0.5;
 		}
-		else if (tab[i][j + 1] = "joueur")
+		else if (tab[i][j + 1] == 'j')
 		{
 			printf("Mouvement impossible, vous ne pouvez pas prendre la place d'un adversaire\n");
 		}		
 }
-*/
+
 
 int bondir(int point_d_energie, char** tab, int orientation, Joueur joueur, int i, int j)
 {
@@ -504,19 +501,14 @@ int bondir(int point_d_energie, char** tab, int orientation, Joueur joueur, int 
 
 	if (orientation == 3)
 	{
-		if (tab[i + 2][j] == '-')
-		{
-			printf("Mouvement impossible, vous etes en face des limites du plateau\n");
-			
-		}
-		else if (tab[i + 2][j] == 'x')
+		if (tab[i + 2][j] == 'e')
 		{
 			tab[i][j] = tab[i + 2][j];
 			point_d_energie = point_d_energie - 2;
 			return tab[i][j];
 			
 		}
-		else if (tab[i + 2][j] == '1' || tab[i + 2][j] == '2' || tab[i + 2][j] == '3' || tab[i + 2][j] == '4')
+		else if (tab[i + 2][j] != 'e')
 		{
 			printf("Mouvement impossible, vous ne pouvez pas prendre la place d'un adversaire\n");
 		}		
@@ -550,15 +542,6 @@ char looking(int orientation)
 	return data;
 }
 */
-int mouvement_horizontal(int point_de_mouvement)
-{
-	int tmp_data = 0;
-	for (int i = 0; i < point_de_mouvement; i++)
-	{
-		tmp_data = i;
-	}
-	return tmp_data ;
-}
 
 bool test_quantiter_point_action(int point_d_action, int commande)
 {
@@ -569,162 +552,147 @@ bool test_quantiter_point_action(int point_d_action, int commande)
 	return false;
 }
 
-char commande(int point_d_energie, float point_d_action,  char *action, int **tab, int orientation, char *action_joueur, int i, int j)
+char commande(int point_d_energie, float point_d_action,  char *action, char **tab, int orientation, char *action_joueur, int i, int j, Joueur joueur)
 {
 	if(strcmp(action,"forward") == 0)
-		deplacer_haut(tab[i][j], point_d_action, orientation);
-
+		forward(tab, point_d_action, orientation,joueur,i,j);
 	else if(strcmp(action, "backward") == 0)
-		deplacer_bas(tab[i][j], point_d_action, orientation);
-
-	else if(strcmp(action == "leftfwd")==0)
-		deplacer_gauche(tab[i][j], point_d_action, orientation);
-
+		backward(tab, point_d_action, orientation,joueur,i,j);
+	else if(strcmp(action, "leftfwd")==0)
+		leftfwd(tab, point_d_action, orientation,joueur,i,j);
 	else if(strcmp(action, "rightfwd")==0)
-		deplacer_droite(tab[i][j], point_d_action, orientation);
-
-	else if(strcmpt(action, "right")==0)
+		rightfwd(tab, point_d_action, orientation,joueur,i,j);
+	else if(strcmp(action, "right")==0)
 	{
 		point_d_action = point_d_action - 0.5;
-		pivoter_right(orientation);
+		right(orientation);
 	}
-
 	else if(strcmp(action, "left") == 0)
 	{
 		point_d_action = point_d_action - 0.5;
-		pivoter_left(orientation);
+		left(orientation);
 	}
-
 	else if(strcmp(action, "looking") == 0) 
 		return 1;
-
 	else if(strcmp(action, "gather") == 0 )
 		point_d_action = point_d_action - 1;
-
-	else if(action == "watch")
-		???
-
 	else if(action == "attack")
 	{
 		vague_energie(point_d_action, point_d_energie, orientation);
 	}
-
 	else if(action == "selfid")
-		point_d_action = point_d_action - 1;
-
+            selfid(joueur);
 	else if(action == "selfstats")
-		return 42;
-
+		selfstats(joueur);
 	else if(action == "inspect")
 		point_d_action = point_d_action - 0.5;
-
 	else if(action == "next")
 		return point_d_action;
-
 	else if(action == "jump")
-		bondir(point_d_energie, tab[i][j], orientation);
+		bondir(point_d_energie, tab, orientation,joueur,i,j);
 }
-/*
+}
+
 void vague_energie(float point_d_action, int point_d_energie, int orientation, int tab[i][j])
 {
-	
-	if (orientation == 1) //Nord
-	{								//Case gauche										Case droite													Case en face										Case haut
-		if ((tab[i - 1][j - 1] =< '4' || tab[i - 1][j - 1] == "empty") && (tab[i - 1][j + 1] =< '4' || tab[i - 1][j + 1] == "empty") && (tab[i - 1][j] =< '4' || tab[i - 1][j] == "empty") && (tab[i - 2][j] =< '4' || tab[i - 2][j] == "empty"))
-		{
-			
-		}
-		point_d_action = point_d_action - 0.5;
-		point_d_energie--;
-	}
 
-	if (orientation == 2 ) //Est
-	{								//Case gauche										Case droite													Case en face										Case haut
-		if ((tab[i - 1][j + 1] =< '4' || tab[i - 1][j + 1] == "empty") && (tab[i + 1][j + 1] =< '4' || tab[i + 1][j + 1] == "empty") && (tab[i][j + 1] =< '4' || tab[i][j + 1] == "empty") && (tab[i][j + 2] =< '4' || tab[i][j + 2] == "empty"))
-		{
-			//point_d_energie = point_d_energie - 2;
-		}
-		point_d_action = point_d_action - 0.5;
-		point_d_energie--;
-	}
+    if (orientation == 1) 
+    {
+        if ((tab[i - 1][j - 1] == 'e') && (tab[i - 1][j + 1] =< '4' || tab[i - 1][j + 1] == 'e') && (tab[i - 1][j] =< '4' || tab[i - 1][j] == 'e') && (tab[i - 2][j] =< '4' || tab[i - 2][j] == 'e'))
+        {
 
-	if (orientation == 3 ) //Sud
-	{
-		if ((tab[i + 1][j + 1] =< '4' || tab[i + 1][j + 1] == "empty") && (tab[i + 1][j - 1] =< '4' || tab[i + 1][j - 1] == "empty") && (tab[i + 1][j] =< '4' || tab[i + 1][j] == "empty") && (tab[i + 2][j] =< '4' || tab[i + 2][j] == "empty"))
-		{
-			//point_d_energie = point_d_energie - 2;
-		}
-		point_d_action = point_d_action - 0.5;
-		point_d_energie--;
-	}
+        }
+        point_d_action = point_d_action - 0.5;
+        point_d_energie--;
+    }
 
-	if (orientation == 0 ) //Ouest
-	{
-		if ((tab[i + 1][j - 1] =< '4' || tab[i + 1][j - 1] == "empty") && (tab[i - 1][j + 1] =< '4' || tab[i - 1][j - 1] == "empty") && (tab[i][j - 1] =< '4' || tab[i][j - 1] == "empty") && (tab[i][j - 2] =< '4' || tab[i][j - 2] == "empty"))
-		{
-			//point_d_energie = point_d_energie - 2;
-		}
-		point_d_action = point_d_action - 0.5;
-		point_d_energie--;
-	}
+    if (orientation == 2 ) 
+    {								
+        if ((tab[i - 1][j + 1] =< '4' || tab[i - 1][j + 1] == 'e') && (tab[i + 1][j + 1] =< '4' || tab[i + 1][j + 1] == 'e') && (tab[i][j + 1] =< '4' || tab[i][j + 1] == 'e') && (tab[i][j + 2] =< '4' || tab[i][j + 2] == 'e'))
+        {
+
+        }
+        point_d_action = point_d_action - 0.5;
+        point_d_energie--;
+    }
+
+    if (orientation == 3 ) 
+    {
+        if ((tab[i + 1][j + 1] =< '4' || tab[i + 1][j + 1] == 'e') && (tab[i + 1][j - 1] =< '4' || tab[i + 1][j - 1] == 'e') && (tab[i + 1][j] =< '4' || tab[i + 1][j] == 'e') && (tab[i + 2][j] =< '4' || tab[i + 2][j] == 'e'))
+        {
+
+        }
+        point_d_action = point_d_action - 0.5;
+        point_d_energie--;
+    }
+
+    if (orientation == 0 ) 
+    {
+        if ((tab[i + 1][j - 1] =< '4' || tab[i + 1][j - 1] == 'e') && (tab[i - 1][j + 1] =< '4' || tab[i - 1][j - 1] == 'e') && (tab[i][j - 1] =< '4' || tab[i][j - 1] == 'e') && (tab[i][j - 2] =< '4' || tab[i][j - 2] == 'e'))
+        {
+
+        }
+        point_d_action = point_d_action - 0.5;
+        point_d_energie--;
+    }
 }
 
 char decla_tab(FILE* fichier_tab)
 {
-	
-	int i = 0;
-	while(!feof(fichier_tab) && i < 20)
-	{
-		
-		int j = 0;
 
-		// On récupere la ligne courante du fichier
-		fgets(szbuff,20,fichier_tab);
-		
-		// On decoupe la ligne selon le charactere de séparation SEP (" ")
-		token = strtok(szbuff,SEP);
+    int i = 0;
+    while(!feof(fichier_tab) && i < 20)
+    {
 
-		// On lit les éléments découpés un à un et on les stocke dans le tableau Tab
-		while(token != NULL && j < CMAX)
-		{
-			// On stocke la valeur lue dans le tableau
-			Tab[i][j] = atoi(token);
-			// On incremente le compteur des ordonnées
-			j++;
-		}
-		
-		// On incrémente le compteur des abscisses
-		i++;
-	}
-	fclose(fichier_tab);
-	return Tab[i][j];
+        int j = 0;
+
+
+        fgets(szbuff,20,fichier_tab);
+
+
+        token = strtok(szbuff,SEP);
+
+
+        while(token != NULL && j < CMAX)
+        {
+
+            Tab[i][j] = atoi(token);
+
+            j++;
+        }
+
+
+        i++;
+    }
+    fclose(fichier_tab);
+    return Tab[i][j];
 }
 
 char nom_rand()
 {
-	if 
+    if 
 }
 
 void main(int argc, char const *argv[])
 {
-	struct Plateau plat;
-	struct joueur J_1, J_2, J_3, J_4;
+    struct Plateau plat;
+    struct joueur J_1, J_2, J_3, J_4;
 
-	//FILE* fichier_map =  fopen("map.txt", "r+");
-	//int tab[15][15] = decla_tab(fichier_map);
 
-	int map[12][12] = plat.tab[12][12];
-	J_1.map[1][1];
-	J_2.map[10][1];
-	J_3.map[10][10];
-	J_4.map[1][10];
 
-	while(J_1.point_d_energie != 0 && J_2.point_d_energie != 0 && J_3.point_d_energie != 0 && J_4.point_d_energie != 0 )
-	{
-		sleep_clock(9000);
 
-		//point_d_energie = point_d_energie - 2;
-	}
-	return 0;
+    int map[12][12] = plat.tab[12][12];
+    J_1.map[1][1];
+    J_2.map[10][1];
+    J_3.map[10][10];
+    J_4.map[1][10];
+
+    while(J_1.point_d_energie != 0 && J_2.point_d_energie != 0 && J_3.point_d_energie != 0 && J_4.point_d_energie != 0 )
+    {
+        sleep_clock(9000);
+
+
+    }
+    return 0;
 }
 */
